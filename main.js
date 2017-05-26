186abd9226489d9938d8338f2df1374f086074d8
Rodrigo López Dato <rolodato@rolodato.com>
2017-05-26 11:55:13 -0300

    Stupid fix

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const getCertificate = require('./lib');
 module.exports = (args) => {
     return getCertificate(args).then(certs => {
         console.log(`\nSuccess! Go to https://gitlab.com/${certs.repository}/pages and create/re-create domain(s) with the following settings:\n`);
-        console.log(`Domain(s): ${certs.domain.join(', ')}\n`);
+        console.log(`Domain(s): ${certs.domains.join(', ')}\n`);
         console.log(`Certificate (PEM):\n${certs.cert}\n${certs.ca}\n`);
         console.log(`Key (PEM):\n${certs.key}`);
     }).catch(err => {
